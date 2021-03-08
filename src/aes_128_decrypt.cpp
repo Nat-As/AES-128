ecdce95824c097d1985db7ca483661b73715ae05
James Andrews <jandrews7348@floridapoly.edu>
2021-03-08 12:19:49 -0500

    Add files via upload

--- src/aes_128_decrypt.cpp
+++ src/aes_128_decrypt.cpp
@@ -1,1 +1,28 @@
-// aes_128_decrypt.cpp
+#include "../include/aes_128_decrypt.h"
+
+// Decryption function
+byte *decrypt_aes_128(byte *ciphertext, byte *key) {
+    byte *plaintext = NULL;
+    // Fill this function
+    return plaintext;
+}
+
+// IS-box ( Inverse substitute a single byte )
+byte inverse_substitute_byte(byte byte_to_substitute) {
+    // Fill this function
+}
+
+// Inverse substitute the bytes of the state array
+void inverse_substitute_bytes(byte *byte_array) {
+    // Fill this function
+}
+
+// Inverse Shift rows
+void inverse_shift_rows(byte *byte_array) {
+    // Fill this function
+}
+
+// Inverse Mix Columns
+void inverse_mix_columns(byte *byte_array) {
+    // Fill this function
+}
