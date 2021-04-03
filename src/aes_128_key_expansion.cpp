7f3a248f28e73bb30cf8b88c392d25b24fec11d0
James Andrews <jandrews7348@floridapoly.edu>
2021-04-03 17:54:58 -0400

    Update aes_128_key_expansion.cpp

--- src/aes_128_key_expansion.cpp
+++ src/aes_128_key_expansion.cpp
@@ -1,13 +1,28 @@
+#include <iostream>
 #include "../include/aes_128_key_expansion.h"
+
+using namespace std;
 
 // Circular Left Shift (Rotate Left)
 void circular_left_shift(byte *byte_word) {
     // Fill this function
+	byte temp[4];
+	// Shift
+	temp[0] = byte_word[4];
+	for (int i=1; i<4; i++){
+		temp[i] = byte_word[i-1];
+	}
+	// Assign values back to byte_word
+	for (int i=1; i<4; i++){
+		byte_word[i] = temp[i];
+	}
+	cout << byte_word;
 }
 
 // Add Round Constant
 void add_round_constant(byte *byte_word, unsigned char round_number) {
     // Fill this function
+	
 }
 
 
@@ -24,7 +39,7 @@ byte *g_function(byte *byte_word)
 byte *get_round_key(byte *key_bytes, unsigned char round_number) {
     byte *round_key = new byte[16];
     // Fill this function
-    // Note: Don't forget to delete the return og the g_function once you are done using it
+    // Note: Don't forget to delete the return of the g_function once you are done using it
     return round_key;
 }
 
