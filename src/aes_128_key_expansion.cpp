0520f623e98e58dfa38c07df100138b3277de161
James Andrews <jandrews7348@floridapoly.edu>
2021-04-05 10:53:18 -0400

    Needs S_Box function

--- src/aes_128_key_expansion.cpp
+++ src/aes_128_key_expansion.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "../include/aes_128_key_expansion.h"
+#include "include/aes_128_key_expansion.h"
 
 using namespace std;
 
@@ -16,21 +16,48 @@ void circular_left_shift(byte *byte_word) {
 	for (int i=1; i<4; i++){
 		byte_word[i] = temp[i];
 	}
-	cout << byte_word;
+	//cout << byte_word;
 }
 
 // Add Round Constant
 void add_round_constant(byte *byte_word, unsigned char round_number) {
-    // Fill this function
-	
+    // Not ideal, but get's the job done.
+	switch (round_number){
+		case 1:
+			*byte_word = 0x01;
+		case 2:
+			*byte_word = 0x02;
+		case 3:
+			*byte_word = 0x04;
+		case 4:
+			*byte_word = 0x08;
+		case 5:
+			*byte_word = 0x10;
+		case 6:
+			*byte_word = 0x20;
+		case 7:
+			*byte_word = 0x40;
+		case 8:
+			*byte_word = 0x80;
+		case 9:
+			*byte_word = 0x1B;
+		case 0x0A:
+			*byte_word = 0x36;
+	}
 }
 
 
 // The g function of the AES key expansion
 byte *g_function(byte *byte_word)
 {
-    byte *g_return_word=new byte[4];
+    byte *g_return_word = new byte[4];
     // Fill this function
+	circular_left_shift(*byte_word);
+	// XOR S Box results with Round Constant (Concatenate with last round key)
+	for (unsigned char round_number = 0; round_number <= 10; round_number++){
+		*g_return_word = *g_return_word | S_Box(*byte_word)^add_round_constant(*byte_word, round_number);
+	}
+	
     return g_return_word;
 }
 
@@ -40,6 +67,9 @@ byte *get_round_key(byte *key_bytes, unsigned char round_number) {
     byte *round_key = new byte[16];
     // Fill this function
     // Note: Don't forget to delete the return of the g_function once you are done using it
+	for (unsigned char round_number = 0; round_number <= 10; round_number++){
+		*round_key = S_Box(*key_bytes)^add_round_constant(*key_bytes, round_number);
+	}
     return round_key;
 }
 
